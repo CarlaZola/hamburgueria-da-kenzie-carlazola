cc421b211a3fe56f403c471e5bf7567f952f48e3
carla <carla.zollaa@gmail.com>
2023-02-01 18:05:27 -0300

    feat: global style and preference theme

---
+++ src/pages/Dashboard/dashboard.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const StyledDashboard = styled.main`
+        background-color: ${({theme}) => theme.colors.colorWhite};
+
+`
+
+export default StyledDashboard
---
+++ src/styles/theme.js
@@ -0,0 +1,25 @@
+
+export const ThemeLigth = {
+    colors: {
+        colorPrimary: "#27AE60",
+        colorPrimary100: "#2cc36b",
+        colorPrimary50: "#93D7AF",
+        colorSecundary:"#EB5757",
+        colorGrey100: "#333333",
+        colorGrey50: "#828282",
+        colorGrey20: "#E0E0E0",
+        colorGrey0: "#F5F5F5",
+        colorWhite: " #ffffff",
+    }
+}
+
+
+export const ThemeDark ={
+    colors: {
+        colorGrey100: "##F5F5F5",
+        colorGrey50: "##E0E0E0",
+        colorGrey20: "#828282",
+        colorGrey0: "#333333",
+        colorWhite: "#000000",
+    }
+}

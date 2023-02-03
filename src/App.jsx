d894a55307db73e0c952af74cfbb7ad83acfff59
4lysson-a <4ly.alcantara@gmail.com>
2023-02-03 15:37:46 -0300

    fix: arrumando bug '-'

--- src/App.jsx
+++ src/App.jsx
@@ -1,132 +1,132 @@
-import { useState, useEffect } from "react";
-import RoutesMain from "./routes";
-import api from "./services/api"
+import { useState, useEffect } from 'react';
+import RoutesMain from './routes';
+import api from './services/api';
 import { ThemeProvider } from 'styled-components';
-import { ThemeLigth, ThemeDark } from "./styles/theme";
-import Global from "./styles/globalStyles";
+import { ThemeLigth, ThemeDark } from './styles/theme';
+import Global from './styles/globalStyles';
 
 // import MaskGroup from "./assets/MaskGroup.svg";
 // import MaskGroupDark from "./assets/MaskGroupDark.svg";
 
+const themePreference = JSON.parse(localStorage.getItem('@themePreference'));
+const productsInLocal = localStorage.getItem('@products');
+
 function App() {
+	const [products, setProducts] = useState([]);
+	const [currentSale, setCurrentSale] = useState(productsInLocal ? JSON.parse(productsInLocal) : []);
+	const [theme, setTheme] = useState(themePreference ? themePreference : 'ligth');
+	const [loading, setLoading] = useState(false);
+	const [cart, setCart] = useState(false);
 
-  const productsInLocal = localStorage.getItem("@products")
-  const themePreference = JSON.parse(localStorage.getItem("@themePreference"))
+	useEffect(() => {
+		setLoading(true);
+		async function getProdcuts() {
+			try {
+				const response = await api.get('/products');
 
-  const [products, setProducts] = useState([])
-  const [currentSale, setCurrentSale] = useState(productsInLocal ? JSON.parse(productsInLocal) : [])
-  const [theme, setTheme] = useState(themePreference ? themePreference : "ligth")
-  const [loading, setLoading] = useState(false)
-  const [cart, setCart] = useState(false)
+				const responseIfQuantities = response.data.map((item) => {
+					const filterAddItem = JSON.parse(productsInLocal)?.filter((product) => product.id === item.id)[0];
+					if (filterAddItem) {
+						return {
+							...filterAddItem,
+							quantities: Number(filterAddItem.quantities)
+						};
+					} else {
+						return { ...item, quantities: 0 };
+					}
+				});
 
+				setProducts(responseIfQuantities);
+			} catch (error) {
+				console.log(error);
+			} finally {
+				setLoading(false);
+			}
+		}
+		getProdcuts();
+	}, []);
 
+	// console.log(currentSale);
 
-  useEffect(() =>{
-    setLoading(true)
-    async function getProdcuts(){
-      try {
-        const response = await api.get("/products")
+	useEffect(() => {
+		// console.log('currentSale', currentSale);
+		localStorage.setItem('@products', JSON.stringify(currentSale));
+	}, [currentSale]);
 
-        const responseIfQuantities = response.data.map((item) => {
-          const filterAddItem = JSON.parse(productsInLocal)?.filter((product) => product.id === item.id)[0]
-          if(filterAddItem){
-            console.log(filterAddItem)
-            return {...filterAddItem, quantities : Number(filterAddItem.quantities)}
-          }
-          else{
-            return {...item, quantities: 0}
-          }
-        })
-        console.log(responseIfQuantities)
-        setProducts(response.data)
+	useEffect(() => {
+		localStorage.setItem('@themePreference', JSON.stringify(theme));
+	}, [theme]);
 
-      } catch (error) {
-          console.log(error)
-      }
-      finally{
-        setLoading(false)
-      }
-    }
-    getProdcuts()
-  }, [])
-  
-  console.log(currentSale)
+	// console.log(products);
 
-  // useEffect(() => {
-  //   return 
-  // }, [])
-  
-  useEffect(() => {
-    localStorage.setItem("@themePreference", JSON.stringify(theme))
-  }, [theme])
+	function addProductsInCart(product) {
+		if (!currentSale.some((currentProduct) => currentProduct.id === product.id)) {
+			product.quantities = 1;
+			setCurrentSale([...currentSale, product]);
+		} else {
+			const response = currentSale.map((current) => {
+				if (current.id === product.id) {
+					product.quantities++;
+					return product;
+				}
+				return current;
+			});
 
-  function addProductsInCart(product){
-  
-      if(!currentSale.some((currentProduct) => currentProduct.id === product.id)){  
-        product.quantities = 1
-        setCurrentSale([...currentSale, product])    
-        localStorage.setItem("@products", JSON.stringify(currentSale))    
-      }
-      else{      
-          product.quantities++
-          setCurrentSale([...currentSale])
-          localStorage.setItem("@products", JSON.stringify(currentSale))
-         
-      }
-  }
-  // console.log(productsInLocal)
-  // console.log(JSON.parse(productsInLocal))
-  // console.log(currentSale)
-  
-  function removeProductsInCart(product){
-      if(product.quantities > 1){
-        if(currentSale.some((currentProduct) => currentProduct.id === product.id)){
-          product.quantities = product.quantities - 1
-          setCurrentSale([...currentSale])
-        }
-      }
-      else{
-        const newCurrentSale = currentSale.filter((currentProduct) => currentProduct.id !== product.id)
-        setCurrentSale([...newCurrentSale])
-      }
-  
-  }
+			console.log(response);
+			setCurrentSale(response);
+		}
+	}
+	// console.log(productsInLocal)
+	// console.log(JSON.parse(productsInLocal))
+	// console.log(currentSale)
 
-  function removeAllPrductsSame(product){
-      const newCurrentSale = currentSale.filter((currentProduct) => currentProduct.id !== product.id)
-      setCurrentSale([...newCurrentSale])
-  }
+	function removeProductsInCart(product) {
+		if (product.quantities > 1) {
+			if (currentSale.some((currentProduct) => currentProduct.id === product.id)) {
+				product.quantities = product.quantities - 1;
+				setCurrentSale([...currentSale]);
+			}
+		} else {
+			const newCurrentSale = currentSale.filter((currentProduct) => currentProduct.id !== product.id);
+			setCurrentSale([...newCurrentSale]);
+		}
+	}
 
-  // function sumOfAmounts(){
-  //   const newCurrent = [...currentSale]
-  //   const quantitiesInCart = newCurrent.map((sale) => sale.quantities)
-  //   console.log(quantitiesInCart)
-  // }
-  
-  // sumOfAmounts()
+	function removeAllPrductsSame(product) {
+		const newCurrentSale = currentSale.filter((currentProduct) => currentProduct.id !== product.id);
+		setCurrentSale([...newCurrentSale]);
+	}
 
-  function clearCart(){
-      return setCurrentSale([])
-  }
+	// function sumOfAmounts(){
+	//   const newCurrent = [...currentSale]
+	//   const quantitiesInCart = newCurrent.map((sale) => sale.quantities)
+	//   console.log(quantitiesInCart)
+	// }
 
-  // function getProductBySearch(letters){
-  //   return 
-  // } 
-  return (
-    <>
-      <Global/>
-      <ThemeProvider theme={theme === "ligth" ?  ThemeLigth : ThemeDark}>
-          <RoutesMain          
-          setTheme={setTheme}
-          products={products}
-          addProductsInCart={addProductsInCart}
-          removeProductsInCart={removeProductsInCart}
-          removeAllPrductsSame={removeAllPrductsSame}
-          theme={theme}
-          />       
-      </ThemeProvider>
-    </>
-  )
+	// sumOfAmounts()
+
+	function clearCart() {
+		return setCurrentSale([]);
+	}
+
+	// function getProductBySearch(letters){
+	//   return
+	// }
+	return (
+		<>
+			<Global />
+			<ThemeProvider theme={theme === 'ligth' ? ThemeLigth : ThemeDark}>
+				<RoutesMain
+					setTheme={setTheme}
+					products={products}
+					addProductsInCart={addProductsInCart}
+					removeProductsInCart={removeProductsInCart}
+					removeAllPrductsSame={removeAllPrductsSame}
+					theme={theme}
+				/>
+			</ThemeProvider>
+		</>
+	);
 }
 
-export default App
+export default App;
